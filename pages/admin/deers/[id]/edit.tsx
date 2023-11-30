ad2a375cd04f866af471c2eb5343ac2ca6e88e35
John Young <theroad2eldorado@gmail.com>
2023-11-30 00:13:09 -0500

    removes evenly

--- pages/admin/deers/[id]/edit.tsx
+++ pages/admin/deers/[id]/edit.tsx
@@ -394,7 +394,6 @@ export default function EditDeer({ data, isNew }: Props) {
                 defaultValue='Remainder'
                 options={[
                   { value: 'Remainder', label: 'Remainder of meat' },
-                  { value: 'Evenly distribute', label: 'Evenly distribute with specialty meat' },
                   { value: 'None', label: 'None - All specialty meat' },
                 ]}
               ></Select>
