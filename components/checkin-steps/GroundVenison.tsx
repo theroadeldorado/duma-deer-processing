45fac694206a83d3a678a340a018f4fc88542c44
TR2E <john@skycatchfire.com>
2025-09-06 17:07:19 -0400

    ✨ Update description for GroundVenison component to clarify that all ground burger will be used for specialty meats, enhancing user understanding.

--- components/checkin-steps/GroundVenison.tsx
+++ components/checkin-steps/GroundVenison.tsx
@@ -53,7 +53,11 @@ export default function GroundVenison(props: StepProps) {
 
   const groundVenisonAmountOptions = [
     { value: 'Remainder', label: 'Remainder of meat', description: 'Ground from remaining meat after cuts and specialty meat options' },
-    { value: 'None - All specialty meat', label: 'No burger - All specialty meat', description: 'All ground will be used for specialty meats' },
+    {
+      value: 'None - All specialty meat',
+      label: 'No burger - All specialty meat',
+      description: 'All ground burger will be used for specialty meats',
+    },
   ];
 
   const handleGroundVenisonSelect = (value: string) => {
